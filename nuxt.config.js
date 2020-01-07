2778acf538a5c9de85c166aa0dd8f7aac74fac32
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2020-01-07 21:24:20 +0700

    Add plugin vue barcode scanner

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,6 +30,7 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    { src: '~plugins/vue-barcode-reader.js', mode: 'client' }
   ],
   /*
   ** Nuxt.js dev-modules
