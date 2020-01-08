587921fbcee5ea32bc1a4e6b013a2afbef2db777
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2020-01-08 10:10:40 +0700

    Add navbar component

---
+++ components/base/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="base-navbar" class="relative">
+    <div class="flex flex-wrap items-center bg-primary py-2 lg:px-16">
+      <div class="w-full lg:w-1/5 flex items-center px-2 lg:justify-center">
+        <nuxt-link to="/" class="h-12 w-12 rounded-full bg-white flex items-center justify-center">
+          <img src="~/assets/images/logo.png" alt="Logo Application" class="h-8"/>
+        </nuxt-link>
+      </div>
+
+      <div
+        class="hidden w-full lg:w-4/5 lg:flex flex-col lg:flex-row justify-end items-center"
+      >
+        <ul
+          class="flex flex-col lg:flex-row lg:items-center leading-none text-sm text-white font-bold p-0 m-0"
+        >
+          <li class="inline-block mx-2 mt-4 lg:mt-0">
+            <nuxt-link to="/">BERANDA</nuxt-link>
+          </li>
+
+					<li class="inline-block mx-2 mt-4 lg:mt-0">
+            <nuxt-link to="/scanner">PEMINDAI</nuxt-link>
+          </li>
+
+					<li class="inline-block mx-2 mt-4 lg:mt-0">
+            <nuxt-link to="/">BANTUAN</nuxt-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="h-1"></div>
+  </div>
+</template>
+
+<script>
+export default {
+	name: 'Navbar'
+}
+</script>
