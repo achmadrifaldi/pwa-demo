f9eaf6da7560aa06793f30770285c5ca1905d3cf
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2020-01-08 10:10:47 +0700

    Add blank layout

---
+++ layouts/blank.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="layout-blank" class="bg-gray-300 h-screen">
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
