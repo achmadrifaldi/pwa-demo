116bc506b96a5049065673f9e1c91a02d338d8e3
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2020-01-08 12:59:01 +0700

    Add card component

---
+++ components/base/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-white rounded-lg border p-6">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SectionContent',
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
