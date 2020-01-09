10550900a9976a2be0e2271cd9e84e47f82d3905
Achmad Rifaldi <achmadrifaldi@makmal.xyz>
2020-01-09 09:07:12 +0700

    Remove slot link

--- components/base/SectionContent.vue
+++ components/base/SectionContent.vue
@@ -1,31 +1,24 @@
 <template>
   <div class="bg-white p-6">
-		<div class="flex">
-			<div class="w-2/3">
-				<h1 class="font-bold text-xl text-gray-800">{{ title }}</h1>
-				<p class="text-sm text-gray-700">{{ subtitle }}</p>
-			</div>
-			<div class="w-1/3">
-				<slot name="link"></slot>
-			</div>
-		</div>
-		<slot />
+    <h1 class="font-bold text-xl text-gray-800">{{ title }}</h1>
+    <p class="text-sm text-gray-700">{{ subtitle }}</p>
+    <slot />
   </div>
 </template>
 
 <script>
 export default {
-	name: 'SectionContent',
+  name: 'SectionContent',
 
-	props: {
-		title: {
-			type: String,
-			default: ''
-		},
-		subtitle: {
-			type: String,
-			default: ''
-		}
-	}
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    }
+  }
 }
 </script>
